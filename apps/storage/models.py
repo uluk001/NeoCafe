65e557dace115ee209405c980ec34e19c02ce9d3
Hacker <Hacker@MacBook-Pro-2.local>
2023-11-16 12:40:27 +0600

    added date_of_arrival to Ready-Made-Product model

--- apps/storage/models.py
+++ apps/storage/models.py
@@ -66,6 +66,7 @@ class ReadyMadeProduct(models.Model):
     image = models.ImageField(upload_to="images")
     minimal_limit = models.DecimalField(max_digits=10, decimal_places=2)
     name = models.CharField(max_length=100)
+    date_of_arrival = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
