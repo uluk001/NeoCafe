040e9c70252053925bd4ec811e6aa04053dbf47f
Ulukman Zhyrgalbek uulu <uluk001@MacBook-Pro-2.local>
2023-11-29 21:36:16 +0600

    added menu, get_low_stock_ingredients_in_branch, get_a_list_of_ingredients_and_their_quantities_in_specific_branch endpoints

--- config/settings.py
+++ config/settings.py
@@ -99,21 +99,14 @@ CLOUDINARY_STORAGE = {
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
