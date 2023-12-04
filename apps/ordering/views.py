f8a35c1dbb3e490d8a8694073be9e174f35110cf
Ulukman Zhyrgalbek uulu <uluk001@MacBook-Pro-2.local>
2023-12-04 12:35:01 +0600

    updated create-order/

--- apps/ordering/views.py
+++ apps/ordering/views.py
@@ -4,17 +4,42 @@ Module for ordering views.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from utils.menu import check_if_items_can_be_made
 from apps.storage.models import Item
 from apps.ordering.serializers import OrderSerializer
 from apps.ordering.services import create_order
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
 class CreateOrderView(APIView):
     """
     View for orders.
     """
-    
+
+    @swagger_auto_schema(
+        operation_summary="Creates order.",
+        operation_description="User must be authenticated, items must be not empty, total price must be greater than 0, spent bonus points must be greater than or equal to 0.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'total_price': openapi.Schema(type=openapi.TYPE_NUMBER, description='Total price of the order'),
+                'spent_bonus_points': openapi.Schema(type=openapi.TYPE_INTEGER, description='Bonus points spent on the order'),
+                'items': openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'item': openapi.Schema(type=openapi.TYPE_INTEGER, description='ID of the item'),
+                            'quantity': openapi.Schema(type=openapi.TYPE_INTEGER, description='Quantity of the item'),
+                        }
+                    ),
+                    description='List of items in the order',
+                ),
+            },
+            required=['total_price', 'spent_bonus_points', 'items'],
+        ),
+    )
+
     def post(self, request):
         """
         Creates order.
@@ -24,6 +49,8 @@ class CreateOrderView(APIView):
             total_price = serializer.validated_data['total_price']
             spent_bonus_points = serializer.validated_data['spent_bonus_points']
             items = serializer.validated_data['items']
+            if not items:
+                return Response({"items": ["This field must not be empty."]}, status=status.HTTP_400_BAD_REQUEST)
 
             order = create_order(
                 user_id=request.user.id,
