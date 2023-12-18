395dd53dd9a8fea980295aa30240caf356c700ba
Ulukman Zhyrgalbek uulu <uluk001@MacBook-Pro-2.local>
2023-12-18 12:29:46 +0600

    added add-item-to-order/, remove-order-item/

--- apps/ordering/urls.py
+++ apps/ordering/urls.py
@@ -4,11 +4,14 @@ Module for defining the urls for the ordering app.
 from django.urls import path
 from apps.ordering.views import (
     CreateOrderView, ReorderView,
-    ReorderInformationView,
+    ReorderInformationView, AddItemToOrderView,
+    RemoveOrderItemView,
 )
 
 urlpatterns = [
     path('create-order/', CreateOrderView.as_view()),
     path('reorder/', ReorderView.as_view()),
     path('reorder-information/', ReorderInformationView.as_view()),
+    path('add-item-to-order/', AddItemToOrderView.as_view()),
+    path('remove-order-item/', RemoveOrderItemView.as_view()),
 ]
